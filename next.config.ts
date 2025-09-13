8bc044e7b3a920d1c3a20edf554a67436b5399c4
Sanjay B <sanjayjothi2000@gmail.com>
2025-09-13 22:54:14 +0530

    changes: config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: process.env.NODE_ENV === "production" ? "export" : undefined, // enables static export only for production
-  basePath: process.env.NODE_ENV === "production" ? "/nimble-insights-web" : undefined, // basePath only for production/GH Pages
-  assetPrefix: process.env.NODE_ENV === "production" ? "/nimble-insights-web" : undefined, // assetPrefix for GitHub Pages
-  trailingSlash: process.env.NODE_ENV === "production" ? true : false, // trailing slash for GitHub Pages compatibility
   images: {
     unoptimized: true, // disables next/image optimization (not supported on GH Pages)
   },

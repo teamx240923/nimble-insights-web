c22fa1bc455abce262ecf12fa8ae33bdd2c49d4c
Sanjay B <sanjayjothi2000@gmail.com>
2025-09-14 14:45:34 +0530

    Updates

---
+++ _next/static/hm7rZTKWbMFkCbchWUFU9/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/db821bf9df2b3f4e.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/nimble-insights-web//_next/:path+",
+        "destination": "/nimble-insights-web/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()

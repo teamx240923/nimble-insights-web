58aab122868d967925ff6e98a5df1ed25550809e
Sanjay B <sanjayjothi2000@gmail.com>
2025-09-17 10:01:25 +0530

    changes: next config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: isProd ? "/nimble-insights-web" : "",
-  assetPrefix: isProd ? "/nimble-insights-web/" : "",
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
